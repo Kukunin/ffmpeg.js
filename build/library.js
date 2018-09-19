bfa1ef792f4da8f09d58c2a4797973068ec0de5e
Sergey Kukunin <sergey.kukunin@gmail.com>
2018-09-19 14:59:41 +0200

    Write binary directly to Module['stdoutBinary']

--- build/library.js
+++ build/library.js
@@ -1,12 +1,17 @@
 mergeInto(LibraryManager.library, {
-  emscripten_stdin_async__deps: ['$EmterpreterAsync'],
-  emscripten_stdin_async: function(buf, size) {
+  emscripten_binary_read__deps: ['$EmterpreterAsync'],
+  emscripten_binary_read: function(buf, size) {
     return EmterpreterAsync.handle(function(resume) {
       Module['stdinAsync'](size, function(data) {
         var finalSize = Math.min(size, data.length);
-        Module.HEAPU8.set(data.subarray(0, finalSize), buf);
+        Module['HEAPU8'].set(data.subarray(0, finalSize), buf);
         resume(function() { return finalSize; });
       });
     });
   },
+
+  emscripten_binary_write: function(buf, size) {
+    Module['stdoutBinary'](Module['HEAPU8'].subarray(buf, buf + size));
+    return size;
+  }
 });
